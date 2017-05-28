b0ec02751e5c31e3343e667d6334ffc063a1925c
hiadachi <b1504ha@aiit.ac.jp>
2017-05-28 05:33:38 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,35 @@
 module SessionsHelper
-    # 渡されたユーザーでログインする
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-    # 現在ログイン中のユーザーを返す (いる場合)
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
-    # ユーザーがログインしていればtrue、その他ならfalseを返す
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+
+ # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
   
   # 現在のユーザーをログアウトする
@@ -19,4 +38,14 @@ module SessionsHelper
     @current_user = nil
   end
   
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
